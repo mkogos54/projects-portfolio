9a8a1fb5bbefe2544f174cfec677f77dbba8f69c
Margaret Kogos <107774602+mkogos54@users.noreply.github.com>
2025-09-20 22:08:25 -0400

    updated to match May 2025 code

--- Thesis/Filters.h
+++ Thesis/Filters.h
@@ -3,9 +3,9 @@
 
 bool initialize_filters();
 
-void render_bandpass(float audio_l, float audio_r);
-void render_lowpass(float audio_l, float audio_r);
-void render_highpass(float audio_l, float audio_r);
-void render_lowandhighpass(float audio_l, float audio_r);
+void render_bandpass(float audio_l, float audio_r, float cutoff);
+void render_lowpass(float audio_l, float audio_r, float cutoff);
+void render_highpass(float audio_l, float audio_r, float cutoff);
+void render_lowandhighpass(float audio_l, float audio_r, float HiCutoff, float LoCutoff);
 
 #endif 
